b86d9d5f68eb304380d0b98334976bed8d94e484
vladradu <mevladradu@gmail.com>
2024-04-30 11:29:10 +0300

    extract method for trimming the data

--- models/classification/predict.py
+++ models/classification/predict.py
@@ -21,15 +21,19 @@ def load_models():
     return loaded_models
 
 
-def make_prediction(input_data_path, models):
-    input_data = pd.read_csv(input_data_path)
-    trim_data = input_data.drop('name', axis=1)
-    trim_data['respectsISNT'] = trim_data['respectsISNT'].astype(int)
-    predictions = np.array([model.predict(trim_data) for model in models.values()])
+def make_prediction(trimmed_data, models):
+    predictions = np.array([model.predict(trimmed_data) for model in models.values()])
 
     # Majority voting
     final_prediction = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=predictions)
     return final_prediction
+
+
+def trim_data(input_data_path):
+    input_data = pd.read_csv(input_data_path)
+    trimmed_data = input_data.drop('name', axis=1)
+    trimmed_data['respectsISNT'] = trimmed_data['respectsISNT'].astype(int)
+    return trimmed_data
 
 
 def main():
@@ -39,8 +43,10 @@ def main():
 
     input_data_csv = sys.argv[1]
     input_data_path = f"{CSV_DIR}/{input_data_csv}"
+    trimmed_data = trim_data(input_data_path)
+
     models = load_models()
-    prediction = make_prediction(input_data_path, models)
+    prediction = make_prediction(trimmed_data, models)
     diagnosis = "Glaucoma" if prediction else "Healthy"
     print(f"Predicted diagnosis: {diagnosis}")
 
